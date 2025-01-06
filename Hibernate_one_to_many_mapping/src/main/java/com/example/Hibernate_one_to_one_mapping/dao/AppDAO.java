c329330319b054116aa6eb5a0ee672e1a92235f7
Md. Ashiq Uddin Pranto <pranto@Mds-MacBook-Air.local>
2025-01-06 14:31:38 +0600

    updateInstructor, UpdateCourse, DeleteInstructor, deleteCourse

--- Hibernate_one_to_many_mapping/src/main/java/com/example/Hibernate_one_to_one_mapping/dao/AppDAO.java
+++ Hibernate_one_to_many_mapping/src/main/java/com/example/Hibernate_one_to_one_mapping/dao/AppDAO.java
@@ -23,6 +23,13 @@ public interface AppDAO {
 
     Instructor findInstructorByIdJoinFetch(int theId);
 
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
 }
 
 
